125f18ac405e5ffb0c122e7bc04f5e5e54b7aeff
BuildTools <unconfigured@null.spigotmc.org>
2022-05-29 13:37:19 +0400

    try 2

--- src/commands/rs.ts
+++ src/commands/rs.ts
@@ -1,11 +1,12 @@
 import { exec } from "child_process";
 import { ICommand } from "wokcommands";
+import execSh from "exec-sh";
 
 export default {
   category: "Restart",
   description: "Pulls & Restarts BOT",
 
   callback() {
-    exec("git pull");
+    execSh("git pull");
   },
 } as ICommand;
